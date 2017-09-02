0f4a365d529b04cd0b0d13313af1ca8ebe7b75cf
Jair Rosa <jdarosaj@hotmail.com>
2017-09-02 11:41:54 -0300

    Extracting feature: MFCC

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #import numpy as np
 #from hmmlearn import hmm
 #np.random.seed(42)
@@ -133,10 +135,73 @@
 #         print('>>>>>>>>>>>>>><<<<<<<<<<<<<<<<')
 
 import os
+# import pyacoustics
+import argparse
+
+import numpy as np
+from scipy import special, optimize
+import matplotlib.pyplot as plt
+
+from python_speech_features import mfcc
+#from python_speech_features import delta
+#from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+# All values in ms
+WINDOW_SIZE = 25.
+WINDOW_STEP = 10.
+
+
+def extract_mfcc(filename):
+    (rate,sig) = wav.read( filename )
+    
+    #print("Signal: {}".format( WINDOW_SIZE/1000 ))
+
+    #nfft changed to the same number of frame length, because the frame was been truncated
+    mfcc_feat = mfcc(sig, rate, WINDOW_SIZE/1000, WINDOW_STEP/1000, nfft=1103)
+
+    #default inputs (from documentation)
+    #mfcc_feat = mfcc(sig,samplerate=16000,winlen=0.025,winstep=0.01,numcep=13,
+    #             nfilt=26,nfft=512,lowfreq=0,highfreq=None,preemph=0.97, ceplifter=22,appendEnergy=True)
+    
+    #not used for now...
+    #d_mfcc_feat = delta(mfcc_feat, 2)
+    #fbank_feat = logfbank(sig,rate)
+
+    print(mfcc_feat)
+#    return mfcc_feat #return breaks I dont know why yet
+
 
 
 def extract_pitch(filename):
     print( 'Extracting pitch for {}'.format(filename) )
+
+    #I've tried to use pyacoustics without success
+    #pyacoustics.intensity_and_pitch.praat_pi.getPraatPitchAndIntensity()
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(usage=__doc__)
+    parser.add_argument("--order", type=int, default=3, help="order of Bessel function")
+    parser.add_argument("--output", default="plot.png", help="output image file")
+    args = parser.parse_args()
+
+    # Compute maximum
+    f = lambda x: -special.jv(args.order, x)
+    sol = optimize.minimize(f, 1.0)
+
+    # Plot
+    x = np.linspace(0, 10, 5000)
+    plt.plot(x, special.jv(args.order, x), '-', sol.x, -sol.fun, 'o')
+
+    # Produce output
+    plt.savefig(args.output, dpi=96)
+
+
+
+
+
+
+
     return []
 
 def read_all_audio_files_from_path( path ):
@@ -151,12 +216,13 @@ def read_all_audio_files_from_path( path ):
 if __name__ == '__main__':
     print('Starting program...')
 
-    # path_to_read_files = '/root/dev/emotion_speech_recognition/audio_samples/'
-    path_to_read_files = 'C:\dev\emotion_speech_recognition\\audio_sample\\'
+    path_to_read_files = '/root/dev/emotion_speech_recognition/audio_sample/'
+    # path_to_read_files = 'C:\dev\emotion_speech_recognition\\audio_sample\\'
     filenames = read_all_audio_files_from_path(path_to_read_files)
 
     for filename in filenames:
-        pitch = extract_pitch(filename)
+ #       pitch = extract_pitch(filename)
+        extract_mfcc(filename)
 
     # extract_parameters()
     # extract_chromagram("/root/tcc/extraction/audio_samples/teste2-2.wav")
