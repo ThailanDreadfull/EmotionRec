b5329875ffb77a02dd909fac14bc091051f1ec30
Jair <jdarosaj@hotmail.com>
2017-09-03 12:36:47 -0300

    energy ftr

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-#sys.path.append('/root/miniconda2/lib/')
-#from foo import util,bar
-
 import os
 import argparse
 
@@ -15,11 +12,14 @@ from python_speech_features import mfcc
 import scipy.io.wavfile as wav
 
 
+
+
+
 # All values in ms
 WINDOW_SIZE = 25.
 WINDOW_STEP = 10.
 
-
+#### FUNCIONANDO OK
 def extract_mfcc(filename):
     (rate,sig) = wav.read( filename )
     
@@ -39,14 +39,32 @@ def extract_mfcc(filename):
     print(mfcc_feat)
 #    return mfcc_feat #return breaks I dont know why yet
 
-#import librosa
 
-def extract_energy(filename):
-    pass
+
+
+import yaafelib
+import h5py
+
+def extract_energy_with_yaafe(filename):
     
-    #mtd from librosa
+    #yaafe -r 44100 -f "mfcc: MFCC blockSize=1024 stepSize=512" test.wav
+    h5py.run_tests()
+    #f = h5py.File('myfile.hdf5','r')
+
+
+import librosa
+def extract_energy(filename):
+    
+    # 1ms = 22 samples, so 25ms = 550 samples
+    sig, sample_rate = librosa.load( filename )
+    
     #Compute root-mean-square (RMS) energy for each frame, either from the audio samples y or from a spectrogram S.
-    #rmse([y, S, frame_length, hop_length, ...])
+    energy = librosa.feature.rmse( y=sig, frame_length=550 )
+    #librosa.rmse([audio_signal, spectrogram_magnitude, frame_length, hop_length, ...])
+    
+    #print( str( len(sig) ) )
+    print(energy)
+
 
 def extract_pitch(filename):
     print( 'Extracting pitch for {}'.format(filename) )
@@ -96,12 +114,13 @@ if __name__ == '__main__':
     filenames = read_all_audio_files_from_path(path_to_read_files)
 
     for filename in filenames:
- #       pitch = extract_pitch(filename)
-        extract_mfcc(filename)
+        #pitch = extract_pitch(filename)
+        #extract_mfcc(filename)
+        extract_energy(filename)
+        break
 
     # extract_parameters()
     # extract_chromagram("/root/tcc/extraction/audio_samples/teste2-2.wav")
     
     print('Ending program...')
-
 
